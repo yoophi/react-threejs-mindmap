5a963abe3de63383811d41ec9ad738265abd39af
Pyunghyuk Yoo <yoophi@gmail.com>
2025-04-09 00:45:57 +0900

    feat: update styles and dependencies for threejs mindmap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
 function App() {
-  return <div>hello, world</div>;
+  const divRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    divRef.current.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    }
+
+    animate();
+  }, [divRef]);
+  return <div className='App' ref={divRef} />;
 }
 
 export default App;
